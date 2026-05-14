162e23a4d2421c8baf66347829199a3c4d9571dc
Lucas <lucas.bacco.2025127@my.istec.pt>
2026-05-14 21:59:43 +0100

    Desenvolvimento da pagina inicial

--- library/views.py
+++ library/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Recurso
 
-# Create your views here.
+
+def pagina_inicial(request):
+    """Pagina principal: lista todos os recursos."""
+    recursos = Recurso.objects.all()
+    contexto = {
+        'recursos': recursos,
+        'total': recursos.count(),
+    }
+    return render(request, 'library/pagina_inicial.html', contexto)
